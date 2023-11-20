11e6b8102a19eda9e196ae5762073e6570294875
Joao <Joao.Alcides@pmtw10-020895.taruma.sp.gov.br>
2023-11-20 16:21:14 -0300

    primeiro

---
+++ WebApplication1/Models/Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    [Table("Alunos")]
+    public class Aluno
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string nome { get; set; }
+        [StringLength(100)]
+        public string diciplina { get; set; }
+
+        public float nota1 { get; set; }
+        public float nota2 { get; set; }
+    }
+
+
+}
+
+
